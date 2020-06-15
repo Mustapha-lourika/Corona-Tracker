c2e401eb318f0b4c4cf7ec89aad4f4e50755cfbd
Mustapha Lourika <mostaphalourika@gmail.com>
2020-06-16 00:33:44 +0100

    Updates

---
+++ precache-manifest.a2eb0b85262ca5c8de0140799e76baab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758510d1888223c1a7617489c2ab1235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68f7fb4a73b091d75493",
+    "url": "/static/css/main.a69a8a50.chunk.css"
+  },
+  {
+    "revision": "f2bfa1dd4916cd12a7d2",
+    "url": "/static/js/2.5342f3e1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.5342f3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f7fb4a73b091d75493",
+    "url": "/static/js/main.d72850be.chunk.js"
+  },
+  {
+    "revision": "55ea17ac4beba09c9ec3",
+    "url": "/static/js/runtime-main.1d1eec46.js"
+  },
+  {
+    "revision": "52a96b5309e16fc6385929fc96310afc",
+    "url": "/static/media/corona.52a96b53.jpg"
+  }
+]);
